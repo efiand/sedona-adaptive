f8f0374ea6dad645939141cb97bdfb65eae6d6fa
Андрей Ефимов <efiand@ya.ru>
2019-04-13 22:21:25 +0300

    fix img

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,10 +2,6 @@
 // Диспетчер подключений для сборки javascript-кода из фрагментов с помощью gulp-include
 
 (() => {
-  // Плагины общего назначения
-  const functions = {};
-  //=require ../../gulpfile.js/functions/**/*.both.js
-
   // Клиентские модули
   //=require modules/**/*.js
 
